18a912e3521ef27f44ed67acffa0c2835b902a6e
Guida <margarida.s.martins00@gmail.com>
2020-12-29 19:25:14 +0000

    fix certificate attributes

--- server/server.py
+++ server/server.py
@@ -311,10 +311,9 @@ class MediaServer(resource.Resource):
             return b''
 
     def check_user_cert_chain(self,cert_chain):
-        """
+
         if not self.validate_attributes(cert_chain):
             return False
-        """
         if not self.validate_crl(cert_chain):
             return False
 
@@ -345,41 +344,36 @@ class MediaServer(resource.Resource):
             or key_usage.crl_sign or key_usage.encipher_only or key_usage.decipher_only):
             return False
 
-        elif not (c.not_valid_before< datetime.now()< c.not_valid_after):
-            return False
-        
-        elif not (c.subject.get_attributes_for_oid(NameOID.ORGANIZATION_NAME).value=='Cartão de Cidadão'):
+        if not (c.not_valid_before< datetime.now()< c.not_valid_after):
             return False
 
+        if not (c.subject.get_attributes_for_oid(NameOID.ORGANIZATION_NAME)[0].value=='Cartão de Cidadão'):
+            return False
+        
         c=x509.load_der_x509_certificate(cert_chain[1].encode("latin"))
         key_usage=c.extensions.get_extension_for_oid(ExtensionOID.KEY_USAGE).value
         if ( key_usage.digital_signature or key_usage.content_commitment or key_usage.key_encipherment 
             or key_usage.data_encipherment or key_usage.key_agreement or not key_usage.key_cert_sign  
-            or not  key_usage.crl_sign or key_usage.encipher_only or key_usage.decipher_only):
-            return False
-        """
-        elif not (c.not_valid_before< datetime.now()< c.not_valid_after):
+            or not  key_usage.crl_sign):
             return False
         
-        elif not (c.subject.get_attributes_for_oid(NameOID.ORGANIZATION_NAME).value=='Cartão de Cidadão'):
+        if not (c.not_valid_before< datetime.now()< c.not_valid_after):
+            return False
+        
+        if not (c.subject.get_attributes_for_oid(NameOID.ORGANIZATION_NAME)[0].value=='Cartão de Cidadão'):
             return False
         
         c=x509.load_der_x509_certificate(cert_chain[2].encode("latin"))
         key_usage=c.extensions.get_extension_for_oid(ExtensionOID.KEY_USAGE).value
         if (key_usage.digital_signature or key_usage.content_commitment or key_usage.key_encipherment 
             or key_usage.data_encipherment or key_usage.key_agreement or not key_usage.key_cert_sign  
-            or not  key_usage.crl_sign or key_usage.encipher_only or key_usage.decipher_only):
-            print("SUBCC")
+            or not  key_usage.crl_sign ):
+            return False
+        if not (c.not_valid_before< datetime.now()< c.not_valid_after):
+            return False
+        if not (c.subject.get_attributes_for_oid(NameOID.ORGANIZATION_NAME)[0].value=='SCEE - Sistema de Certificação Electrónica do Estado'):
             return False
 
-        elif not (c.not_valid_before< datetime.now()< c.not_valid_after):
-            print("SUBhhhhhhhhhhh")
-            return False
-        elif not (c.subject.get_attributes_for_oid(NameOID.ORGANIZATION_NAME).value=='SCEE - Sistema de Certificação Electrónica do Estado'):
-            print("Root name")
-            return False
-        print("Pass")
-        """
         return True
 
     def check_cert_signature(self,cert_chain):
